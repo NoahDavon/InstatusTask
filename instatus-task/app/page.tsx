2c179e1b4b44bda858aefaac54cbdf3d5430d574
Mohammed <MohammedE.Fahmy@outlook.com>
2024-03-07 12:22:53 +0200

    Some styling

--- instatus-task/app/page.tsx
+++ instatus-task/app/page.tsx
@@ -2,22 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <table className=" table-auto">
-        <thead>
+    <div className=" w-full">
+      <table className="table-auto w-full text-left border-separate border border-spacing-0 overflow-hidden border-opacity-0 rounded-lg">
+        <thead className=" bg-[#F5F5F5] text-[#616161]">
           <tr>
             <td colSpan={4}>
               {/* search bar here */}
             </td>
           </tr>
           <tr>
-            <th>ACTOR</th>
-            <th>ACTION</th>
-            <th colSpan={2}>DATE</th>
+            <th className=" p-4 pr-0">ACTOR</th>
+            <th className=" p-4 pr-0">ACTION</th>
+            <th className=" p-4 pr-0" colSpan={2}>DATE</th>
           </tr>
         </thead>
-        
       </table>
-    </div>
+      </div>
   );
 }
