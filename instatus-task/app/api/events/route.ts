581a05f7abce8de105bd4af9147e31058bbdf6fe
Mohammed <MohammedE.Fahmy@outlook.com>
2024-03-08 19:14:47 +0200

    GET route

--- instatus-task/app/api/events/route.ts
+++ instatus-task/app/api/events/route.ts
@@ -1,9 +1,32 @@
+import { PrismaClient } from '@prisma/client/extension';
 import { type NextRequest } from 'next/server'
- 
-export function GET(request: NextRequest) {
-  const prismaClient = 
+
+const DOCS_PER_PAGE = 6;
+const prisma = new PrismaClient();
+export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
-  const type = searchParams.get('type');
+  const type = searchParams.get('type') as string;
   const query = searchParams.get('query');
+  const page = parseInt(searchParams.get("page")?? "1") - 1;
+  const events = await prisma.event.findMany({
+    skip: page * DOCS_PER_PAGE,
+    take: DOCS_PER_PAGE,
+    where: {
+      [type]: query
+    }
+  }
+  )
+  const nextPage = await prisma.event.findFirst({
+    skip: (page+1) * DOCS_PER_PAGE,
+    where: {
+      [type]: query
+    }
+  })
+  const nextPageExists: boolean = nextPage;
+  const res = {events, nextPageExists};
+  return new Response(JSON.stringify(res),
+    {
+      status: 200
+    })
   // query is "hello" for /api/event?query=hello
 }
